1d6bc3f63177909dc24a14474e02e0231b8c59e1
wangying <wangying@stargraph.cn>
2023-11-26 12:41:03 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,25 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const mockList = require('./mock/index.js')
+const app = new Koa()
+const router = new Router()
+
+async function getRes(fn, ctx) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			const res = fn(ctx)
+			resolve(res)
+		}, 1000)
+	})
+}
+mockList.forEach((item) => {
+	const { url, method, response } = item
+	router[method](url, async (ctx) => {
+		// const res = response()
+		const res = await getRes(response, ctx) // 模拟网络请求的加载状态，1s
+		ctx.body = res // 输入结果
+	})
+})
+
+app.use(router.routes())
+app.listen(3001) // port 端口
