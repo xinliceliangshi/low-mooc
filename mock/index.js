79376564e08ccbc37adfb1fdd1fd5a5dd2c7e51c
wangying <wangying@stargraph.cn>
2023-11-29 10:49:29 +0800

    feat: 添加登录模拟数据，统计模拟接口等

--- mock/index.js
+++ mock/index.js
@@ -1,10 +1,15 @@
 
 const test = require('./test')
 const question = require('./question')
-
+const user   = require('./user')
+const stat   = require('./stat')
+const answer = require('./answer')
 const mockList = [
     ...test,
-    ...question
+    ...question,
+    ...user,
+    ...stat,
+    ...answer
 ]
 
 module.exports = mockList
